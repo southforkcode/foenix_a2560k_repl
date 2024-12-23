a6db5bc126d7fa2c280ca67305af770eea5661c6
John Magdaleno <johnamagdaleno@gmail.com>
2024-12-23 15:21:34 -0600

    Use lea to load addresses; added bounds check for command look up

--- src/repl.s
+++ src/repl.s
@@ -1,5 +1,8 @@
-;;; simple_repl.s
-
+;;; repl.s
+;   A simple REPL - read execute print loop
+;   Notes:
+;    - Following the vbcc ABI: d0, d1, a0, and a1 are scratch registers.
+;    - This embeds the PGX header requirements directly in source.  
 
 ;;; System calls
 SYSC_EXIT = $00
@@ -16,80 +19,74 @@ SYSC_READL = $12
 
 ;;; SIMPLE REPL
 start:
-    move.l #intro,a0
+    lea intro,a0
     jsr puts
 
 repl:
-    move.l #prompt,a0
+    lea prompt,a0
     jsr puts
 
     ; get user input
-    move.l #buffer,a0
+    lea buffer,a0
     move.l #buffer_end-buffer,d0 ; size of buffer
     jsr gets
     move.l d0,d3 ; save number of bytes received
 
     ; print line break after input
-    move.l #10,d0
-    jsr putc
+    jsr putcr
 
     ; empty line exits
     cmp.l #0,d3
     beq done
 
     ; check match
-    move.l #buffer,a0
+    lea buffer,a0
     jsr match_cmd
     move.l d0,d3
-    cmp.l #-1,d3
+    cmp.l #-1,d3 ; handle error if no command found
     beq .error
 
     ; look up command impl and call
-    ; d0 = index
-    ; TODO: this is dangerous - probably put table size in var to check
-    lsl.l #2,d3 ; d0 = d0 * 4
-    move.l d3,d0
-    jsr puthexl
-    move.b #10,d0
-    jsr putc
-    move.l #command_impls,a0
-    move.l (0,a0,d3),a3
-    move.l a3,d0
-    jsr puthexl
-    move.b #10,d0
-    jsr putc
+    lsl.l #2,d3 ; d3 = d3 * 4 - each entry is a 32-bit pointer
+    ; bounds check offset
+    cmp.l #command_impls_end-command_impls,d3
+    blt .bounds_ok
+    ; bounds check failure, print message and continue
+    lea internal_error,a0
+    jsr puts
+    bra repl
+.bounds_ok:
+    lea command_impls,a0
+    move.l (0,a0,d3),a3 ; a3 <- command_impls + offset + 0
+    ; call address held in a3
     jsr (a3)
     bra repl
 
 .error:
     ; unrecognized command
-    move.l #error,a0
+    lea error,a0
     jsr puts
-    move.l #buffer,a0
+    lea buffer,a0
     jsr puts
-    move.b #10,d0
-    jsr putc
+    jsr putcr
     bra repl
 
 block_impl:
     move.l #command_impls,d0
     jsr puthexl
-    move.b #10,d0
-    jsr putc
+    jsr putcr
     rts
 
 hello_impl:
     move.l #buffer,d0
     jsr puthexl
-    move.b #10,d0
-    jsr putc
+    jsr putcr
     rts
     
 done:
 quit_impl:
-
     ; print goodbye
-    move.l #goodbye,a0
+    lea goodbye,a0
     jsr puts
 
     move.l #SYSC_EXIT,d0
@@ -97,20 +94,18 @@ quit_impl:
     trap #15
 
 ;;; match_cmd - given input buffer match a command in the command table
-;;; %a0 - (IN) address of string buffer (input_ptr)
+;;; %a0 - (IN) address of input buffer (input_ptr)
 ;;; %d0 - (OUT) matched command, -1 == no match (return)
 ;   a1 - temp input pointer (cur_input_ptr)
 ;   a2 - temp command list pointer (cmd_list_ptr)
 ;   d7 - temp current command index (cmd_idx)
 ;   d0 - temp byte from input buffer (inpc)
 ;   d1 - temp byte from command buffer (cmdc)
-;   TODO: add input %a1 which is a pointer to the string array
+;   TODO: add input %a1 which is a pointer to the string array, make this generic
 match_cmd:
-    movem.l d1-d7/a0-a6,-(a7)
-    ; int cmd_idx = 0
-    move.l #0,d7 ; command counter
-    ; char *cmd_list_ptr = &commands
-    move.l #commands,a2 ; current offset into command list
+    movem.l d2-d7/a2-a6,-(a7)
+    clr.l d7 ; int cmd_idx = 0
+    move.l #commands,a2 ; char *cmd_list_ptr = &commands
 .next_cmd:
     ; if (*cmd_list_ptr == 0) return -1 // no match
     cmp.b #0,(a2)
@@ -135,24 +130,28 @@ match_cmd:
     move.l #-1,d7
 .matched:
     move.l d7,d0
-    movem.l (a7)+,d1-d7/a0-a6
+    movem.l (a7)+,d2-d7/a2-a6
     rts
 
+;;; putcr - write carriage return to console
+putcr:
+    move.b #10,d0
+    ; notice this falls through to putc
 ;;; putc - writes a byte to console
 ;;; %d0 - (IN) byte
 putc:
-    movem.l d0-d7/a0-a6,-(a7)
+    movem.l d2-d7/a2-a6,-(a7)
     move.b d0,d2
     move.l #SYSC_WRITEB,d0
     clr.l d1
     trap #15
-    movem.l (a7)+,d0-d7/a0-a6
+    movem.l (a7)+,d2-d7/a2-a6
     rts
 
 ;;; puts - writes a null-terminated string to console
 ;;; %a0 - (IN) pointer to string
 puts:
-    movem.l d0-d7/a0-a6,-(a7) ; preserve non-scratch
+    movem.l d2-d7/a2-a6,-(a7) ; preserve non-scratch
     move.l a0,a3 ; a0 is scratch, move to perm a3
     cmp.l #0,a3
     beq .done ; if a0/a3 are null, exit early
@@ -166,7 +165,7 @@ puts:
     trap #15
     bra .loop
 .done:
-    movem.l (a7)+,d0-d7/a0-a6
+    movem.l (a7)+,d2-d7/a2-a6
     rts
 
 ;;; gets - receives input from console until newline or eob - stores in a0
@@ -175,7 +174,7 @@ puts:
 ;;; (%a0) - (OUT) received bytes, null terminated.
 ;;; %d0.w - (OUT) number of bytes received
 gets:
-    movem.l d1-d7/a0-a6,-(a7)
+    movem.l d2-d7/a2-a6,-(a7)
     move.l d0,d3
     sub.l #1,d3 ; reduce buffer size by 1 for null terminator
     move.l a0,a3 ; preserve input a0 for terminating buffer later
@@ -183,40 +182,40 @@ gets:
     clr.l d1 ; console
     move.l a3,d2
     trap #15
-    ; null terminate buffer
+    ; null terminate input in buffer
     move.b #0,(0,a3,d0)
-    movem.l (a7)+,d1-d7/a0-a6
+    movem.l (a7)+,d2-d7/a2-a6
     rts
 
 ;;; puthexl - display a 32-bit value in hex
 ;;; %d0 - (IN) value to display in hex
 puthexl:
-    movem.l d1-d7/a0-a6,-(a7)
+    movem.l d2-d7/a2-a6,-(a7)
     move.l d0,d2
     lsr.l #8,d0
     lsr.l #8,d0
     jsr puthexw
     move.l d2,d0
     jsr puthexw
-    movem.l (a7)+,d1-d7/a0-a6
+    movem.l (a7)+,d2-d7/a2-a6
     rts
 
 ;;; puthexw - display a 16-bit value in hex
 ;;; %d0 - (IN) value to display in hex
 puthexw:
-    movem.l d1-d7/a0-a6,-(a7)
+    movem.l d2-d7/a2-a6,-(a7)
     move.l d0,d2
     lsr.l #8,d0
     jsr puthexb
     move.l d2,d0
     jsr puthexb
-    movem.l (a7)+,d1-d7/a0-a6
+    movem.l (a7)+,d2-d7/a2-a6
     rts
 
 ;;; puthexb - display a single byte value in hex
 ;;; %d0 - (IN) value to display in hex
 puthexb:
-    movem.l d1-d7/a0-a6,-(a7)
+    movem.l d2-d7/a2-a6,-(a7)
     move.l d0,d3
     ; we print the high nibble first
 .high:
@@ -242,7 +241,7 @@ puthexb:
     move.b d4,d0
     jsr putc
 .done:
-    movem.l (a7)+,d1-d7/a0-a6
+    movem.l (a7)+,d2-d7/a2-a6
     rts
 
 prompt:
@@ -254,20 +253,26 @@ goodbye:
     dc.b "Good bye!",10,0
 error:
     dc.b "Not a command: ",0
+internal_error:
+    dc.b "Internal error!",10,0
 
+; input buffer
 buffer:
     blk.b 512,0
 buffer_end:
 
+;
+; Command tables
+; commands: is an array of strings with the last entry is the empty string
+; command_impls: is an array of function pointers to implementations of commands
 commands:
-quit_cmd: dc.b 'quit', 0
-block_cmd: dc.b 'block', 0
-hello_cmd: dc.b 'hello', 0
-    ; end of list
-    dc.b 0
+    dc.b 'quit', 0
+    dc.b 'block', 0
+    dc.b 'hello', 0
+    dc.b 0 ; end of list
 command_impls:
     dc.l quit_impl
     dc.l block_impl
     dc.l hello_impl
-    dc.l 0
+command_impls_end:
 
